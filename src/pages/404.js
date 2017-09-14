70f07ef122716b148c8d1f2cb8b0bc08d3ddee67
Sai Charan Chinta <charu.iitm@gmail.com>
2017-09-14 20:48:59 +0530

    added 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Image404 from './404.png';
 
-const ErrorPage = () => <div>haha</div>;
+const ErrorPage = () => (
+    <div>
+        <h1>Oops, 404! Its OK, happens to best of us.</h1>
+        <img
+          src={Image404}
+          alt={`404`}
+          style={{
+            float: 'left'
+          }}
+        />
+    </div>
+);
 
 export default ErrorPage;
